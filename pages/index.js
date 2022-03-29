f570a4b437eb2be667ec3bcfcad7108ccbaa2913
Susan Skelding <siouxskelding@icloud.com>
2022-03-29 15:07:44 -0500

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,131 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Lentil Soup</p>
+        <p>
+          <h1>What is Soup?</h1>
+
+          Soup is a primarily liquid food, generally served warm or hot (but may be cool or cold), that is made by combining ingredients of meat or vegetables with stock, milk, or water. Hot soups are additionally characterized by boiling solid ingredients in liquids in a pot until the flavors are extracted, forming a broth. Soups are similar to stews, and in some cases there may not be a clear distinction between the two; however, soups generally have more liquid (broth) than stews.[1]
+
+In traditional French cuisine, soups are classified into two main groups: clear soups and thick soups. The established French classifications of clear soups are bouillon and consommé. Thick soups are classified depending upon the type of thickening agent used: purées are vegetable soups thickened with starch; bisques are made from puréed shellfish or vegetables thickened with cream; cream soups may be thickened with béchamel sauce; and veloutés are thickened with eggs, butter, and cream. Other ingredients commonly used to thicken soups and broths include [2] rice, lentils, flour, and grains; many popular soups also include pumpkin, carrots, potatoes, pig's trotters and bird's nests.
+
+Other types of soup include fruit soups, dessert soups, pulse soups like split pea, cold soups and other styles.
+        </p>
+
+        <h2>
+          What are Lentils?
+        </h2>
+        <p>
+        Lentils remain a staple in Middle Eastern and Indian diets, and are popular in cuisines throughout the world.
+
+As a superfood, they are rich in protein — without the fat or cholesterol of animal sources — and provide B vitamins, magnesium, iron and zinc. In fact, lentils have the second highest protein content of all legumes, after soybeans. Because they are so high in protein, lentils are often a meat substitute in vegetarian diets. Pair them with a whole grain, such as brown rice, and you have a complete vegetarian protein source, meaning all the essential amino acids are present.
+
+The soluble fiber in lentils helps lower cholesterol and may benefit those at risk for heart disease and diabetes. Lentils are also an excellent source of folate, a vitamin that helps the body build new cells. It is an especially important nutrient for women who are either pregnant or planning to become pregnant.
+
+In India, where roughly half of the world's lentils are consumed, cultivation dates back to 2500 B.C. Today, more than 50 different varieties are grown. Nearly every traditional Indian meal includes at least one lentil dish, and they are an important source of nutrients for millions of vegetarians on the subcontinent.
+
+Indian cooks buy lentils hulled and split in order to make dal. This highly spiced sauce or stew is served with rice or flatbread. I pair red lentils, which are sold hulled and split, with Indian spices and potatoes for a dal-inspired soup. They soften and break down almost instantly, so it's only necessary to simmer until the potatoes become tender. Often derided as "mushy," red lentils are ideal for turning soups thick and creamy.
+
+From the Near East, lentils eventually reached Western Europe. Grown in southern France, lentilles du Puy with round green seeds and mottled dark-green hulls became a favored ingredient in French bistro cuisine. They hold their shape during cooking more than other lentil varieties. Braised in red wine with diced vegetables and herbs, the lentils are at once lush and earthy. They make a striking match for roasted salmon glazed with grainy mustard and honey.
+
+Lentils were introduced to the Americas by Spanish and Portuguese explorers in the early 16th century. They did not gain a foothold in American cuisine, however, until World War II, when lentils were promoted as an alternative to meat, thanks to their low cost, wide availability and nutritional quality.
+
+Lentils are as good for the environment as they are for our health. Nitrogen-producing bacteria grow naturally at the roots, enriching the soil.
+
+Though lentils are perfect for relaxed home cooking, it's important to note the different cooking times and textures of the various lentil varieties. Red lentils are the fastest, and firm types such as French and Spanish lentils (called pardina) require more time to soften.
+
+If lentils have been stored longer than a year, they will be drier and will take longer to cook than a fresher batch. Follow your recipe's cooking time, but taste as you go to avoid lentils that are too firm or too mushy. Do not mix newly purchased lentils with the ones you've had in the pantry for a while. The stored lentils are likely to be drier, resulting in uneven cooking. Lentils do not require pre-soaking, but another rule of legume cooking does apply: Salt and acidic ingredients (such as vinegar and lemon) toughen their skins, so wait until lentils are tender before adding them.
+
+Many lentil recipes, as well as package directions, advise you to "pick over" the lentils in search of small rocks and debris. This sounds daunting (and vaguely alarming), but it is quick work. For a fast examination, I pour my lentils into a measuring cup very slowly in order to catch any unsavory bits. To be even more thorough, spread the lentils out on a light surface and look them over until you are satisfied. Do a quick rinse in a colander, and quality control is complete.
+
+Then, start any lentil dish by frying up sausage, bacon or, better yet, Spanish chorizo, and you have the makings of a deeply satisfying meal. Add a generous quantity of hearty greens, such as kale or collards, to create a one-pot dish. The combination of tender, earthy lentils and silky, subtle greens might help you empathize with Esau's unfortunate decision.
+        </p>
+        
+        
+         <h3>That time lentils saved Jesus and The Bible</h3>
+<p><img src="/images/kungfujesus.jpg" alt="Italian Trulli"></img></p> 
+<p>
+Watch for the signposts along the way, most obviously in the brothers’ symbolic names — Esau (“rough one”); Jacob (”heel-holder,” “may God protect you”), in the contrasts of their occupations — Esau the hunter; Jacob the tent-dweller — and in the carefully built characters of their father and mother who also play distinctive roles in the ensuing drama.
+
+The boys’ birth is described in the briefest but most telling way: “The first one emerged red, like a hairy mantle all over, so they named him Esau. Then his brother emerged, holding on to the heel of Esau, so they named him Jacob” (Genesis 25:25-26). As the boys grow up the contrasts between them and their parents’ attitude toward them are further signposts on the way to the birthright scene. Esau is described as a skillful hunter, a man of the [wild] outdoors, while Jacob is a “mild man who stayed in camp.” To Isaac, the exploits of his older son are appealing, especially because Esau brings him wild venison, a change from probably eating the culled sheep or goats of his own herd, and so he favors his elder son. Rebekah loves Jacob—no reason given in the text, and so all the more eloquent a declaration (Genesis 25:27-28).
+
+Lentils in the Bible and the Bowl by the Forward
+Image by Avinoam Danin
+
+Lens culinaris, the lentil plant, Mount Scopus Botanical Garden, Jerusalem.
+
+The birthright scene hits hard, infused as it is with the brownish red of the earth (adama), the color of lentil soup, the color of ruddy Esau himself. He is so consumed with a passion to eat that he can barely get the words out, and so eager to eat that he “gulps” his food, a vernacular verb in the Hebrew usually associated with the feeding of animals. If the text is colored red, it is delivered in hard-driving prose: “Jacob then gave Esau bread and lentil stew; he ate and drank, and he rose and went away. Thus did Esau spurn the birthright.”
+
+Jacob shows more audacity than one would expect from a mild man of the tents, and even more so later, when at Rebekah’s urging he fools his now blind father Isaac (blind physically as well as spiritually) into giving him the first-son’s blessing. The first part of this blessing, the one Esau later receives after bitterly sobbing and pleading with his father, bestows the same material benefits on both sons: “ … the dew of heaven [life-giving moisture] and the fat of the earth,” but in addition, Jacob receives the all-important spiritual blessing of the chosen leader; the superior status of the shepherd life over that of the hunter has been established: “And nations bow to you; Be master over your brothers, And let your mother’s sons bow to you … “ (Genesis 27:29).
+
+But Jacob must earn this blessing and develop the necessary character for his new name, Israel (“one who strives [or “struggles”] with God”), which he does throughout the episodes of conflict and courage ahead.
+
+The fateful stew was made from simmering lentils (adashim), the small, round greenish-brown or reddish seeds of the plant Lens culinaris, in water until they reached the consistency of porridge, hence the expression “a mess of pottage” (a thickened soup). The plant itself is a dainty branched annual, which grows to a height of eighteen inches, its seeds (lentils) growing in pods that hang down from wispy tendrils along the plant’s weak stems. Lentils, along with other nutrient-rich crops such as chickpeas and broad beans have been cultivated for millennia as relatively easily grown legumes, a term based on French and Latin that means literally anything that can be gathered.
+
+High in protein and with a savory, satisfying flavor, lentils have been known as “the poor man’s meat” throughout history, and were the second most important component of the biblical diet after grain. Prepared either from fresh or dried seed, lentils were valued in the biblical kitchen and on the road for their versatility. In addition to using them in nutritious stews, lentils could be mashed into a paste to produce cakes. Lentils were included in the list of foods, such as wheat, barley, parched grain, beans, honey, curds (clabbered milk) and cheese that Barzillai and others brought David and his followers, for they knew that “the troops must have grown hungry … in the wilderness” (II Samuel 17:29).
+
+According to Jewish legend, lentils are mourners’ food because they resemble death: “As the lentil rolls, so death, sorrow and mourning constantly roll about among men, from one to the other.” In everyday biblical and post-biblical life, however, lentils were just common fare. To know that Esau sold his birthright, his very soul, for nothing more than a common food, sheds light on his character.
+
+
+</p>
+
+<h4>
+  More Soup?
+</h4>
+<p><img src="/images/soupme.jpg" alt="Italian Trulli"></img></p> 
+<p>
+Evidence of the existence of soup can be found as far back as about 20,000 BC.[3] Boiling was not a common cooking technique until the invention of waterproof containers (which probably came in the form of clay vessels). Animal hides and watertight baskets of bark or reeds were used before this. To boil the water hot rocks were used. This method was also used to cook acorns and other plants.
+
+The word soup comes from French soupe ("soup", "broth"), which comes through Vulgar Latin suppa ("bread soaked in broth") from a Germanic source, from which also comes the word "sop", a piece of bread used to soak up soup or a thick stew.
+
+The word restaurant (meaning "[something] restoring") was first used in France in the 16th century, to refer to a highly concentrated, inexpensive soup, sold by street vendors, that was advertised as an antidote to physical exhaustion. In 1765, a Parisian entrepreneur opened a shop specializing in such soups. This prompted the use of the modern word restaurant for the eating establishments.
+
+In the US, the first colonial cookbook was published by William Parks in Williamsburg, Virginia, in 1742, based on Eliza Smith's The Compleat Housewife; or Accomplished Gentlewoman's Companion, and it included several recipes for soups and bisques. A 1772 cookbook, The Frugal Housewife, contained an entire chapter on the topic. English cooking dominated early colonial cooking; but as new immigrants arrived from other countries, other national soups gained popularity. In particular, German immigrants living in Pennsylvania were famous for their potato soups. In 1794, Jean Baptiste Gilbert Payplat dis Julien, a refugee from the French Revolution, opened an eating establishment in Boston called "The Restorator", and became known as the "Prince of Soups". The first American cooking pamphlet dedicated to soup recipes was written in 1882 by Emma Ewing: Soups and Soup Making.
+
+Portable soup was devised in the 18th century by boiling seasoned meat until a thick, resinous syrup was left that could be dried and stored for months at a time.
+Commercial soup became popular with the invention of canning in the 19th century, and today a great variety of canned and dried soups are on the market.
+
+Canned
+Canned soup can be condensed, in which case it is prepared by adding water (or sometimes milk) or it can be "ready-to-eat", meaning that no additional liquid is needed before eating. Condensed soup (invented in 1897 by John T. Dorrance, a chemist with the Campbell Soup Company[4][5]) allows soup to be packaged into a smaller can and sold at a lower price than other canned soups. The soup is usually doubled in volume by adding a "can full" of water or milk, about 10 US fluid ounces (300 ml). The "ready-to-eat" variant can be prepared by simply heating the contents of the can on a kitchen stove or in a microwave oven, rather than actually cooking anything. Such soups can be used as a base for homemade soups, with the consumer adding anything from a few vegetables to eggs, meat, cream or pasta.
+
+Since the 1990s, the canned soup market has burgeoned, with non-condensed soups marketed as "ready-to-eat", so they require no additional liquid to prepare.[citation needed] Microwaveable bowls have expanded the "ready-to-eat" canned soup market even more, offering convenience (especially in workplaces), and making for popular lunch items. In response to concerns over the negative health effects of excessive salt intake, some soup manufacturers have introduced reduced-salt versions of popular soups.[6]
+
+Today, Campbell's Tomato (introduced in 1897), Cream of Mushroom, and Chicken Noodle (introduced in 1934) are three of the most popular soups in America. Americans consume approximately 2.5 billion bowls of these three soups alone each year.[4] Other popular brands of soup include Progresso.
+
+Dried
+
+instant soup in a powder form
+Dry soup mixes are sold by many manufacturers, and are reconstituted with hot water; other fresh ingredients may then be added.
+
+The first dried soup was bouillon cubes; the earlier meat extract did not require refrigeration, but was a viscous liquid.
+
+East Asian-style instant noodle soups include ramen and seasonings, and are marketed as a convenient and inexpensive instant meal, requiring only hot water for preparation.[7] While North American ones tend to have a powder pack only, instant noodles sold in East Asia commonly include a pack of dried vegetables too.
+
+Western-style dried soups include vegetable, chicken base, potato, pasta and cheese flavors.
+
+Types
+
+Soup course
+For a more comprehensive list, see List of soups.
+In French cuisine, soup is often served before other dishes in a meal. In 1970, Richard Olney gave the place of the entrée in a French full menu: "A dinner that begins with a soup and runs through a fish course, an entrée, a sorbet, a roast, salad, cheese and dessert, and that may be accompanied by from three to six wines, presents a special problem of orchestration".
+
+
+</p>
+<h5>
+Lentil Soup Recipe
+</h5>
+<img src="/images/recipe.jpg" alt="Italian Trulli"></img>
+
+
+      </section>
+    </Layout>
+  )
+}
